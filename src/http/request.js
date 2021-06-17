7d9cc2bddaffc21cdcf3ac564075aa68661619cb
jay-lin26 <z64666760@163.com>
2021-06-17 19:37:54 +0800

    端口号修改

--- src/http/request.js
+++ src/http/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://139.224.63.20:5000/",
+  baseURL: "http://139.224.63.20:3000/",
   headers: { Version: "1.0.1" },
   timeout: 5000,
 });
