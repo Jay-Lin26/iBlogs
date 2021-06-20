b3f5ffd06efec420126f74189aaa64056defd23e
jay_Lin26 <z64666760@163.com>
2021-06-20 18:06:55 +0800

    up

--- src/http/api.js
+++ src/http/api.js
@@ -3,7 +3,7 @@ import service from "./request.js";
 //获取验证码
 export const getCode = function (data) {
   return service({
-    url: "member/verification",
+    url: "member/verification/",
     method: "get",
     params: { email: data },
   });
@@ -11,7 +11,7 @@ export const getCode = function (data) {
 //登录
 export const login = function (data) {
   return service({
-    url: "member/login",
+    url: "member/login/",
     method: "post",
     data,
   });
@@ -19,7 +19,7 @@ export const login = function (data) {
 //注册
 export const register = function (data) {
   return service({
-    url: "member/register",
+    url: "member/register/",
     method: "post",
     data,
   });
@@ -28,7 +28,7 @@ export const register = function (data) {
 //首页文案
 export const index = function () {
   return service({
-    url: "article/index",
+    url: "article/index/",
     method: "get",
   });
 };
@@ -36,7 +36,7 @@ export const index = function () {
 //博客详情
 export const article_detail = function ( data ) {
   return service({
-    url: "article/detail",
+    url: "article/detail/",
     method: "get",
     params: { article_id: data }
   });
