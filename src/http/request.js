ec3e39933ffda3ba6e0c31e9f5a31217e46ca448
jay_Lin26 <z64666760@163.com>
2021-06-23 22:18:35 +0800

    域名变更

--- src/http/request.js
+++ src/http/request.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Vue from "vue";
 
 const service = axios.create({
-  baseURL: "http://139.224.63.20:3000/",
-  // baseURL: "http://127.0.0.1:5000/api/v1.0/",
+  baseURL: "https://api.iblogs.ltd/",
+  // baseURL: "http://127.0.0.1:5000/",
   headers: { Version: "1.0.1" },
   timeout: 10000,
 });
